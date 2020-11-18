b84591e349e49f4431ead22cd15c87e7184e3b49
Clement Chausserie Lapree <clement.chausserielapree@gmail.com>
2020-11-18 14:34:19 +0000

    added text to fucntions

--- clem_toolbox/eda.py
+++ clem_toolbox/eda.py
@@ -10,6 +10,9 @@ import missingno as mn
 
 def missing_value_viz(df, shape='matrix'):
     # return a bar representation of missing values and correlation between missing values
+    # print('''
+    #   \n Dataset missing values: \n
+    #   ''')
     if shape=='all':
         mn.matrix(df, color=(142/255, 187/255, 217/255))
         mn.heatmap(df)
@@ -19,16 +22,23 @@ def missing_value_viz(df, shape='matrix'):
         mn.heatmap(df)
 
 def numerical_features_viz(df):
-  num_data = df._get_numeric_data()
-  for feature in num_data.columns:
-      print(f'\n {feature}')
-      fig, ((ax0, ax1, ax2)) = plt.subplots(1,3,figsize=(20,4))
-      sns.histplot(data=num_data, x=feature, kde=True, ax=ax0).set_title(f'{feature}_Distribution')
-      sns.boxplot(data=num_data, x=feature, ax=ax1).set_title(f'{feature}_Boxplot')
-      ax2=sm.qqplot(num_data[feature],line='s',ax=ax2)
+    # print('''
+    #   \n Numerical features distribution: \n
+    #   ''')
+    num_data = df._get_numeric_data()
+    for feature in num_data.columns:
+        print(feature)
+        fig, ((ax0, ax1, ax2)) = plt.subplots(1,3,figsize=(20,4))
+        sns.histplot(data=num_data, x=feature, kde=True, ax=ax0).set_title(f'{feature}_Distribution')
+        sns.boxplot(data=num_data, x=feature, ax=ax1).set_title(f'{feature}_Boxplot')
+        ax2=sm.qqplot(num_data[feature],line='s',ax=ax2)
+        plt.show
 
 
 def correlation_heat_map(df):
+    # print('''
+    #   \n Feature correlation: \n
+    #   ''')
     corrs = df.corr()
     # Set the default matplotlib figure size:
     fig, ax = plt.subplots(figsize=(12, 12))
@@ -46,16 +56,9 @@ def correlation_heat_map(df):
 
 
 def vizualise_data(df, shape='matrix'):
-
-    print('''
-      \n Dataset missing values: \n
-      ''')
     missing_value_viz(df, shape=shape)
-    print('''
-      \n Numerical features distribution: \n
-      ''')
+    plt.show
     numerical_features_viz(df)
-    print('''
-      \n Feature correlation: \n
-      ''')
+    plt.show
     correlation_heat_map(df)
+    plt.show
