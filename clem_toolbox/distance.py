3f474a7aae863dc4663a53dac208c19f592135e2
Clement Chausserie Lapree <clement.chausserielapree@gmail.com>
2021-02-22 12:24:04 +0100

    corrected distance.py identation and test import

--- clem_toolbox/distance.py
+++ clem_toolbox/distance.py
@@ -20,9 +20,9 @@ def haversine(lon1, lat1, lon2, lat2):
 
 
 if __name__ == "__main__":
-# Le Wagon location
-lat1, lon1 = 48.865070, 2.380009
-#Insert your coordinates from google maps here
-#lat2, lon2 = x, y
-distance = haversine(lon1, lat1, lon2, lat2)
-print(distance)
+    # Le Wagon location
+    lat1, lon1 = 48.865070, 2.380009
+    #Insert your coordinates from google maps here
+    #lat2, lon2 = x, y
+    distance = haversine(lon1, lat1, lon2, lat2)
+    print(distance)
--- tests/test_distance.py
+++ tests/test_distance.py
@@ -1,10 +1,10 @@
 
 import pandas as pd
 # Import from our lib
-from clem_toolbox.lib import haversine
+from clem_toolbox.distance import haversine
 
 def test_distance_type():
-  lat1, lon1 = 48.865070, 2.380009
-  lat2, lon2 = 48.03509454612089, -3.523117322968529
-  assert type(haversine(lon1, lat1, lon2, lat2)) == float
-  assert haversine(lon1, lat1, lon2, lat2) == 444.9210795014478
+    lat1, lon1 = 48.865070, 2.380009
+    lat2, lon2 = 48.03509454612089, -3.523117322968529
+    assert type(haversine(lon1, lat1, lon2, lat2)) == float
+    assert haversine(lon1, lat1, lon2, lat2) == 444.9210795014478
